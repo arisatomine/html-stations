8110af36a1ec930cd6d1b75544fe52755cfe9844
arisatomine <s2222026@stu.musashino-u.ac.jp>
2023-07-26 22:18:36 +0900

    station13clear

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,11 +1,9 @@
 data = "global data";
 
-// このreturnDataに代入される関数の形をアロー関数に変えて"global data"が表示されるようにしてください
-// ↓関数の中の処理は変更しないでください
-const returnThisData = function () {
+// アロー関数に変更
+const returnThisData = () => {
   return this.data;
 };
-// ↑関数の中の処理は変更しないでください
 
 const object = {
   data: "object data",
